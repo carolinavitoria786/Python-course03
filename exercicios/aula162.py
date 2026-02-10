e4c8776bdb13a8bf9e489393fdc690d7c4926bad
Carolina Vitoria Oliveira <carolinavitoria786@gmail.com>
2026-02-10 08:41:41 -0300

    2026-02-10

--- exercicios/aula162.py
+++ exercicios/aula162.py
@@ -15,3 +15,37 @@ produtos = [
 
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópiaa profunda)
+
+def porcentagem(valor):
+    numero = valor * 1.1
+    return numero
+
+def produtos_ordenados_por_nome(nome_lista):
+    ordem = sorted(nome_lista, key=lambda nome: nome['nome'], reverse = True)
+    return ordem
+
+def produtos_ordenados_por_preco(preco_lista):
+    ordem_valor = sorted(preco_lista, key=lambda preco: preco['preco'])
+    return ordem_valor
+    
+
+aumento = [{'nome': p['nome'], 'preco': porcentagem(p['preco'])} for p in produtos]
+nome = produtos_ordenados_por_nome(produtos)
+valor = produtos_ordenados_por_preco(produtos)
+    
+# print(f'Preços com aumento de 10% :{aumento} \n'
+#       f'Ordenados por nome:{nome} \n'
+#       f'Ordenados por valor: {valor}'
+#     )
+
+print("Preços com aumento de 10%:") 
+for p in aumento: 
+    print(f"{p['nome']} - {p['preco']}") 
+    
+print("\nOrdenados por nome:") 
+for p in nome: 
+    print(f"{p['nome']} - {p['preco']}") 
+
+print("\nOrdenados por valor:") 
+for p in valor: 
+    print(f"{p['nome']} - {p['preco']}")
