c93a676104578627dcfdf056b819fdd3ce5c5866
Carolina Vitoria Oliveira <carolinavitoria786@gmail.com>
2026-02-10 06:55:00 -0300

    att 2026-02-10

---
+++ main.py
@@ -0,0 +1,13 @@
+from sys import path
+
+import pacotes.modulo
+from pacotes import modulo
+from pacotes.modulo import *
+
+print(soma(1,2))
+print(pacotes.modulo.soma(1,2))
+print(modulo.soma(1,2))
+print(variavel)
+print(nova_variavel)
+print()
+print(path, sep='\n')
